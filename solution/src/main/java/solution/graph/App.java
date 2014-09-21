1422978c5580a1df2498a620e68c40b701efaed2
damir <diz261087arg>
2014-09-21 23:52:40 +0400

    Create application

---
+++ solution/src/main/java/solution/graph/App.java
@@ -0,0 +1,13 @@
+package solution.graph;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
