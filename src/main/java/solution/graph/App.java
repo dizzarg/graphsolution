96f584b75d2ba430513c2b77c228ffa0f18e6678
damir <diz261087arg>
2014-09-24 23:18:26 +0400

    Добавлен алгоритм

--- src/main/java/solution/graph/App.java
+++ src/main/java/solution/graph/App.java
@@ -2,6 +2,7 @@ package solution.graph;
 
 import java.io.*;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Hello world!
@@ -10,9 +11,33 @@ import java.util.List;
 public class App{
 
     public static void main( String[] args ) throws IOException {
-        List<Trapeze> trapezeList = TrapezeReader.read(new FileReader("data.txt"));
-        System.out.println(trapezeList.size());
-        System.out.println(trapezeList.get(0).checkPoints());
+//        List<Trapeze> trapezeList = TrapezeReader.read(new FileReader("data.txt"));
+//        System.out.println(trapezeList.size());
+//        System.out.println(trapezeList.get(0).checkPoints());
+
+        Random rnd = new Random(System.currentTimeMillis());
+        EdgeWeightedDigraph g = new EdgeWeightedDigraph(10);
+//        for (int i = 0; i < 9; i++) {
+//            for (int j = i+1; j < 10; j++) {
+//                double v = rnd.nextDouble();
+//                double weight = v * v;
+//                g.addEdge(new DirectedEdge(i, j, weight));
+//            }
+//        }
+        g.addEdge(new DirectedEdge(0, 1, 1));
+//        g.addEdge(new DirectedEdge(1, 0, 1));
+//        g.addEdge(new DirectedEdge(0, 2, 2));
+//        g.addEdge(new DirectedEdge(2, 0, 2));
+        g.addEdge(new DirectedEdge(1, 2, 3));
+//        g.addEdge(new DirectedEdge(2, 1, 3));
+        DijkstraAllPairsSP dijkstraAllPairsSP = new DijkstraAllPairsSP(g);
+        double path = dijkstraAllPairsSP.dist(0, 2);
+        for (DirectedEdge edge: dijkstraAllPairsSP.path(0, 2) ){
+            System.out.print(edge.from()+"->"+edge.to() + " ");
+        }
+        System.out.println();
+        System.out.println(path);
+
     }
 }
 
--- src/main/java/solution/graph/DirectedEdge.java
+++ src/main/java/solution/graph/DirectedEdge.java
@@ -1,31 +1,86 @@
 package solution.graph;
-
+/*************************************************************************
+ *  Compilation:  javac DirectedEdge.java
+ *  Execution:    java DirectedEdge
+ *
+ *  Immutable weighted directed edge.
+ *
+ *************************************************************************/
 /**
- * Created by damir on 22.09.14.
- * граф трапеций
+ *  The <tt>DirectedEdge</tt> class represents a weighted edge in an 
+ *  {@link solution.graph.EdgeWeightedDigraph}. Each edge consists of two integers
+ *  (naming the two vertices) and a real-value weight. The data type
+ *  provides methods for accessing the two endpoints of the directed edge and
+ *  the weight.
+ *  <p>
+ *  For additional documentation, see <a href="http://algs4.cs.princeton.edu/44sp">Section 4.4</a> of
+ *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
  */
-public class DirectedEdge {
 
-    Trapeze from;
-    Trapeze to;
+public class DirectedEdge { 
+    private final int v;
+    private final int w;
+    private final double weight;
 
-    public DirectedEdge(Trapeze from, Trapeze to) {
-        this.from = from;
-        this.to = to;
+    /**
+     * Initializes a directed edge from vertex <tt>v</tt> to vertex <tt>w</tt> with
+     * the given <tt>weight</tt>.
+     * @param v the tail vertex
+     * @param w the head vertex
+     * @param weight the weight of the directed edge
+     * @throws IndexOutOfBoundsException if either <tt>v</tt> or <tt>w</tt>
+     *    is a negative integer
+     * @throws IllegalArgumentException if <tt>weight</tt> is <tt>NaN</tt>
+     */
+    public DirectedEdge(int v, int w, double weight) {
+        if (v < 0) throw new IndexOutOfBoundsException("Vertex names must be nonnegative integers");
+        if (w < 0) throw new IndexOutOfBoundsException("Vertex names must be nonnegative integers");
+        if (Double.isNaN(weight)) throw new IllegalArgumentException("Weight is NaN");
+        this.v = v;
+        this.w = w;
+        this.weight = weight;
     }
 
-    // подсчет веса ребра,
-    // растояние между трапециями
-    public int weight(){
-
-        return 0;
+    /**
+     * Returns the tail vertex of the directed edge.
+     * @return the tail vertex of the directed edge
+     */
+    public int from() {
+        return v;
     }
 
-    public Trapeze getFrom() {
-        return from;
+    /**
+     * Returns the head vertex of the directed edge.
+     * @return the head vertex of the directed edge
+     */
+    public int to() {
+        return w;
     }
 
-    public Trapeze getTo() {
-        return to;
+    /**
+     * Returns the weight of the directed edge.
+     * @return the weight of the directed edge
+     */
+    public double weight() {
+        return weight;
+    }
+
+    /**
+     * Returns a string representation of the directed edge.
+     * @return a string representation of the directed edge
+     */
+    public String toString() {
+        return v + "->" + w + " " + String.format("%5.2f", weight);
+    }
+
+    /**
+     * Unit tests the <tt>DirectedEdge</tt> data type.
+     */
+    public static void main(String[] args) {
+        DirectedEdge e = new DirectedEdge(12, 23, 3.14);
+        StdOut.println(e);
     }
 }
